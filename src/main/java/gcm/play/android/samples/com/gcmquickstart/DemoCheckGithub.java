a18bf8ee0f9118d302feca1b0f6599f39c1912cb
Pankaj <pank.kmwt@gmail.com>
2016-01-22 22:54:58 +0530

    Added file to check

---
+++ src/main/java/gcm/play/android/samples/com/gcmquickstart/DemoCheckGithub.java
@@ -0,0 +1,13 @@
+package gcm.play.android.samples.com.gcmquickstart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoCheckGithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_check_github);
+    }
+}
